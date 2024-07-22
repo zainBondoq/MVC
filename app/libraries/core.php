e788d29a28c35f64f6227a96b7ad34771b41f446
zainbondoq <zainbondoq@gmail.com>
2024-07-23 02:10:43 +0300

    loading controller from the URL

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -1,19 +1,41 @@
 <?php
-
-// app core class that creates URL & loads core controller 
-// URL format - /controller/methos/params
-
-class Core {
+  /*
+   * App Core Class
+   * Creates URL & loads core controller
+   * URL FORMAT - /controller/method/params
+   */
+  class Core {
     protected $currentController = 'Pages';
-    protected $currentMethos = 'index';
+    protected $currentMethod = 'index';
     protected $params = [];
-    
-    public function __construct() {
-        $this->getUrl();
+
+    public function __construct(){
+        //print_r($this->getUrl());
+
+        $url = $this->getUrl();
+
+        // Make sure the $url is not null and contains elements
+        if($url && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+            // If exists, set as controller
+            $this->currentController = ucwords($url[0]);
+            // Unset 0 Index
+            unset($url[0]);
+        }
+
+        // Require the controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        // Instantiate controller class
+        $this->currentController = new $this->currentController;
     }
 
     public function getUrl(){
-        echo $_GET['url'];
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+        return []; // Return an empty array if 'url' is not set
     }
 }
-
