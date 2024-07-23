5a936ec4542f0ef1ecdf8646bb2da76aa83a19f8
zainbondoq <zainbondoq@gmail.com>
2024-07-23 03:26:37 +0300

    base controller class

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,13 +1,13 @@
 <?php
 
-class Pages {
+class Pages extends Controller {
     public function __construct() {
 
     
     }
 
     public function index(){
-
+        $this->view('hello');
     }
 
     public function about($id){
--- app/libraries/controller.php
+++ app/libraries/controller.php
@@ -1,2 +1,31 @@
-<!-- the main controller class and each other controller will extends this class -->
+<!-- the main controller class and each other controller will extends this class
+this will loads the model and views
+-->
 
+<?php
+
+class Controller {
+    // load model 
+    public function model($model){
+        // require model file
+        require_once '../models' . $model . 'php';
+
+        // instantiate the model 
+        return new $model();
+    }
+
+
+    //load view 
+
+    public function view ($view, $data = []){
+        // check view file
+        if(file_exists('../views' . $view .'php')){
+            require_once '../views' . $view .'php';
+        }else{
+            die('View does not exsist');
+        }
+    }
+
+
+
+}
